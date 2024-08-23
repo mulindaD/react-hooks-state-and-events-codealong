a15ba3f3216e00dd13bbd3fb324718936fe2a834
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-08-23 11:40:56 +0300

    complete state & events codealong

--- src/components/Toggle.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Toggle() {
-  return <button>OFF</button>;
-}
-
-export default Toggle;
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,17 @@
+
+import { useState } from "react";
+
+
+function Toggle() {
+  const [isOn, setIsOn] = useState(false)
+
+  function handleClick(){
+    setIsOn((isOn) => !isOn)
+  }
+
+  const color = isOn ? "red" : "white"
+
+  return <button style={{ background: color }} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+}
+
+export default Toggle;
